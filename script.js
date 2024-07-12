e8663dad2b211ae3d19e80738e2ce9a6bae3527e
DIAwener <izkuzindaniil@gmail.com>
2024-07-12 15:01:13 +0300

    add points

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ let btn_feed = document.querySelector(".btn-feed")
 let indicator_hearts = document.querySelector(".indicator-hearts")
 let number_hearts = document.querySelector(".number-hearts")
 let bar = document.querySelector(".bar")
+let p = document.createElement("p")
+let body = document.querySelector("body")
 let count_hearts = 0
 let length = 0
 let width = ""
+
 btn_caress.addEventListener('click', function () {
     if (count_hearts + 4 < 100) {
         count_hearts += 5
@@ -13,15 +16,23 @@ btn_caress.addEventListener('click', function () {
         length += 5
         width = length + "%"
         bar.style.height = width
-        if(count_hearts<30){
+        if (count_hearts < 30) {
             bar.style.background = 'red';
-        } 
-        if(count_hearts<60 && count_hearts>30 ){
+        }
+        if (count_hearts < 60 && count_hearts > 30) {
             bar.style.background = 'yellow';
-        } 
-        if(count_hearts >61){
+        }
+        if (count_hearts > 61) {
             bar.style.background = 'green';
-        } 
+        }
+        let new_p = p.cloneNode()
+        new_p.textContent = "+5"
+        new_p.classList.add("point")
+        new_p.style.position = "absolute"
+        new_p.style.left = "19vw"
+        body.prepend(new_p)
+        setTimeout(function(){new_p.remove()}, 500)
+
     } else {
 
     }
@@ -34,15 +45,15 @@ btn_feed.addEventListener('click', function () {
         length += 10
         width = length + "%"
         bar.style.height = width
-        if(count_hearts<30){
+        if (count_hearts < 30) {
             bar.style.background = 'red';
-        } 
-        if(count_hearts<60 && count_hearts>30 ){
+        }
+        if (count_hearts < 60 && count_hearts > 30) {
             bar.style.background = 'yellow';
-        } 
-        if(count_hearts >61){
+        }
+        if (count_hearts > 61) {
             bar.style.background = 'green';
-        } 
+        }
     } else {
 
     }
@@ -55,15 +66,15 @@ function taking_hearts() {
         length -= 1
         width = length + "%"
         bar.style.height = width
-        if(count_hearts<30){
+        if (count_hearts < 30) {
             bar.style.background = 'red';
-        } 
-        if(count_hearts<60 &&count_hearts>30 ){
+        }
+        if (count_hearts < 60 && count_hearts > 30) {
             bar.style.background = 'yellow';
-        } 
-        if(count_hearts >61){
+        }
+        if (count_hearts > 61) {
             bar.style.background = 'green';
-        } 
+        }
     }
     else {
 
